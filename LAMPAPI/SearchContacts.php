c32854695e9717cbef86ba8bb5e16f3208c5c27c
joosnlaurence <jasongotanl@gmail.com>
2025-09-07 02:11:37 -0400

    Change search to use a single field and add ID to JSON output

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -2,36 +2,26 @@
 
 require 'api.php';
 
-// Expected {"firstName":..., "lastName":..., "phone":..., "email":..., "userId":...}
+// Expected {"search":..., "userId":...}
 $input = getJsonRequest();
-
 
 try{
 
     $conn = new PDO("mysql:host=localhost;dbname=COP4331", "TheBeast", "WeLoveCOP4331");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $fields = ['firstName'=>"FirstName",
-                'lastName'=>"LastName",
-                   'phone'=>"Phone",
-                   'email'=>"Email"];
+    $search = trim($input['search']);
+    $userId = $input['userId'];
 
-    $search_str = [];
-    $params = [];
-    foreach($fields as $k=>$col){
-        if(!(trim($input[$k]) === '')){
-            $search_str[] = "`$col` LIKE ?";
-            $params[] = '%' . trim($input[$k]) . '%';
-        }
-    }
-    $search_str[] = "`UserID`=?";
-    $params[] = $input['userId'];
-
-    $stmt = $conn->prepare("SELECT `FirstName`, `LastName`, `Phone`, `Email`
-                                               FROM `Contacts` WHERE ". implode(' AND ', $search_str));
-    $stmt->execute($params);
+    $stmt = $conn->prepare("SELECT * FROM `Contacts` WHERE (`FirstName` LIKE :search OR
+                                                            `LastName` LIKE :search OR
+                                                            CONCAT(`FirstName`,' ',`LastName`) LIKE :search OR
+                                                            `Phone` LIKE :search OR
+                                                            `Email` LIKE :search) AND `UserID`=:userId");
+    $stmt->bindValue(":search", '%' . $search . '%');
+    $stmt->bindValue(":userId", $userId);
+    $stmt->execute();
     $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
     returnDatabaseSearchResult($res);
     
     $conn = null;
@@ -44,13 +34,14 @@ try{
 /**
  * Sends a search result from the database in JSON format.
  * @param array $result An associative array fetched directly from the database.
- * Expects FirstName, LastName, Phone, Email
+ * Expects ID, FirstName, LastName, Phone, Email
  * @return void
  */
 function returnDatabaseSearchResult(array $result){
     $json = ["success"=>true, "result"=>[], "error"=>""];
     foreach($result as $entry){
         $json['result'][] = [
+            'ID'=>$entry['ID'],
             'firstName'=>$entry['FirstName'],
             'lastName'=>$entry['LastName'],
             'phone'=>$entry['Phone'],
